eee8a03637335936d48458ce507ef5a27eb31e8d
Alex Hernandez <62507090+hernandezaw@users.noreply.github.com>
2023-04-26 19:54:22 -0400

    video 107. AJAX: API Fetch

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,8 @@
 "use strict";
+
+// ****************************************************************************
+// AJAX: XMLHttpRequest
+// ****************************************************************************
 (() => {
   const xhr = new XMLHttpRequest();
   const $xhr = document.getElementById("xhr");
@@ -6,13 +10,13 @@
 
   xhr.addEventListener("readystatechange", (e) => {
     if (xhr.readyState !== 4) return;
-    console.log(xhr);
+    // console.log(xhr);
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log("Éxito");
-      console.log(xhr.responseText);
+      // console.log("Éxito");
+      // console.log(xhr.responseText);
       // $xhr.innerHTML = xhr.responseText;
       let json = JSON.parse(xhr.responseText);
-      console.log(json);
+      // console.log(json);
 
       json.forEach((el) => {
         const $li = document.createElement("li");
@@ -36,4 +40,34 @@
 
   //
   xhr.send();
+})();
+
+// ****************************************************************************
+// AJAX: Api Fetch
+// ****************************************************************************
+
+(() => {
+  const $fetch = document.getElementById("fetch");
+  const $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      // console.log(json);
+      // $fetch.innerHTML = json;
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.warn("Estamos en el catch", err);
+      let message = err.statusText || "Ocurrió un error";
+      $fetch.innerHTML = `Error ${err.status}: ${message}`;
+    })
+    .finally(() => {
+      console.log("Esto se ejecutará independientemente del resultado de la promesa.");
+    });
 })();
