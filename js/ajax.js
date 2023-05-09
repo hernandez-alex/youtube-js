272e9c5963160070ba50861bdb23ca007c911939
alex <62507090+hernandezaw@users.noreply.github.com>
2023-05-09 19:41:52 -0400

    video 110. AJAX: Librería Axios + Async-Await

--- js/ajax.js
+++ js/ajax.js
@@ -138,3 +138,34 @@
       console.log("Esto se ejecutará independientemente del resultado Axios")
     );
 })();
+
+(() => {
+  const $axiosAsync = document.getElementById("axios-async");
+  const $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      let json = await res.data;
+      // console.log(res, json);
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axiosAsync.appendChild($fragment);
+    } catch (err) {
+      // console.log(err.response);
+      let message = err.response.statusText || "Ocurrió un error";
+      $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+    } finally {
+      console.log(
+        "Esto se ejecuta independientemente del resultado Axios-Async"
+      );
+    }
+  }
+
+  getData();
+})();
