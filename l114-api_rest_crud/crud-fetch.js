865207af361e8dddb2e364fe6cfd7a62ead7d4ad
alex <62507090+hernandezaw@users.noreply.github.com>
2023-05-19 21:54:04 -0400

    crud fetch culminado

--- l114-api_rest_crud/crud-fetch.js
+++ l114-api_rest_crud/crud-fetch.js
@@ -1,6 +1,121 @@
 const d = document;
 const $table = d.querySelector(".crud-table");
-const $form = d.querySelector("crud-form");
-const $title = d.querySelector("crud-title");
+const $form = d.querySelector(".crud-form");
+const $title = d.querySelector(".crud-title");
 const $template = d.getElementById("crud-template").content;
 const $fragment = d.createDocumentFragment();
+
+const getAll = async () => {
+  try {
+    let res = await fetch("http://localhost:5050/santos");
+    let json = await res.json();
+
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+    console.log(json);
+
+    json.forEach((el) => {
+      $template.querySelector(".name").textContent = el.nombre;
+      $template.querySelector(".constellation").textContent = el.constelacion;
+      $template.querySelector(".edit").dataset.id = el.id;
+      $template.querySelector(".edit").dataset.name = el.nombre;
+      $template.querySelector(".edit").dataset.constellation = el.constelacion;
+      $template.querySelector(".delete").dataset.id = el.id;
+
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+
+    $table.querySelector("tbody").appendChild($fragment);
+  } catch (err) {
+    let message = err.statusText || "Ocurrió un error";
+    $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+  }
+};
+
+d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", async (e) => {
+  if (e.target === $form) {
+    e.preventDefault();
+
+    if (!e.target.id.value) {
+      // Create - POST
+      try {
+        let options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+          body: JSON.stringify({
+            nombre: e.target.nombre.value,
+            constelacion: e.target.constelacion.value,
+          }),
+        };
+
+        let res = await fetch("http://localhost:5050/santos", options);
+        let json = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrió un error";
+        $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+      }
+    } else {
+      // Udate - PUT
+      try {
+        let options = {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+          body: JSON.stringify({
+            nombre: e.target.nombre.value,
+            constelacion: e.target.constelacion.value,
+          }),
+        };
+
+        let res = await fetch(`http://localhost:5050/santos/${e.target.id.value}`, options);
+        let json = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrió un error";
+        alert(`Error ${err.status}: ${message}`);
+      }
+    }
+  }
+});
+
+d.addEventListener("click", async (e) => {
+  if (e.target.matches(".edit")) {
+    $title.textContent = "Editar Santo";
+    $form.nombre.value = e.target.dataset.name;
+    $form.constelacion.value = e.target.dataset.constellation;
+    $form.id.value = e.target.dataset.id;
+  }
+  if (e.target.matches(".delete")) {
+    let isDelete = confirm(`¿Seguro de querer eliminar el Id ${e.target.dataset.id}?`);
+    if (isDelete) {
+      // Delete - DELETE
+      try {
+        let options = {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        };
+
+        let res = await fetch(`http://localhost:5050/santos/${e.target.dataset.id}`, options);
+        let json = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrió un error";
+        $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+      }
+    }
+  }
+});
