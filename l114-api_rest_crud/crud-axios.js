186abb6358c5da3bc2dea57555c7b941c31abd7e
alex <62507090+hernandezaw@users.noreply.github.com>
2023-05-22 08:28:25 -0400

    temp

--- l114-api_rest_crud/crud-axios.js
+++ l114-api_rest_crud/crud-axios.js
@@ -1,6 +1,7 @@
 const d = document;
 const $table = d.querySelector(".crud-table");
-const $form = d.querySelector("crud-form");
-const $title = d.querySelector("crud-title");
+const $form = d.querySelector(".crud-form");
+const $title = d.querySelector(".crud-title");
 const $template = d.getElementById("crud-template").content;
+
 const $fragment = d.createDocumentFragment();
