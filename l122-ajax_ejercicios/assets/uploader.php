c0d6d984698d7fa196dbc6781613a05b0723b30a
alex <62507090+hernandezaw@users.noreply.github.com>
2023-06-03 21:05:49 -0400

    ajuste

--- l122-ajax_ejercicios/assets/uploader.php
+++ l122-ajax_ejercicios/assets/uploader.php
@@ -1,29 +1,29 @@
 <?php
-echo "Hola, respuesta desde el servidor";
-var_dump($_FILES);
+// echo "Hola, respuesta desde el servidor";
+// var_dump($_FILES);
 
-// if(isset($_FILES("file"))){
-//   $name = $_FILES["file"]["name"];
-//   $file = $_FILES["file"]["tmp_name"];
-//   $error = $_FILES["file"]["error"];
-//   $destination = "../files/$name";
-//   $upload = move_uploaded_file($file, $destination);
+if(isset($_FILES("file"))){
+  $name = $_FILES["file"]["name"];
+  $file = $_FILES["file"]["tmp_name"];
+  $error = $_FILES["file"]["error"];
+  $destination = "../files/$name";
+  $upload = move_uploaded_file($file, $destination);
+  
+  if($upload){
+    $res = array(
+        "err" => false,
+        "status" => http_response_code(200),
+        "statusText" => "Archivo $name subido con éxito",
+        "files" => $_FILES["file"]
+      );
+    } else {
+    $res = array(
+      "err" => true,
+      "status" => http_response_code(400),
+      "statusText" => "Error al subir el archivo $name",
+      "files" => $_FILES["file"]
+    );
+  };
 
-//   if($upload){
-//     $res = array(
-//       "err" => false,
-//       "status" => http_response_code(200),
-//       "statusText" => "Archivo $name subido con éxito",
-//       "files" => $_FILES["file"]
-//     );
-//   } else {
-//     $res = array(
-//       "err" => true,
-//       "status" => http_response_code(400),
-//       "statusText" => "Error al subir el archivo $name",
-//       "files" => $_FILES["file"]
-//     );
-//   }
-
-//   echo json_encode($res);
+  echo json_encode($res);
 }
